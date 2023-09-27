670c9ef40ce2d8ce21cef7fce963ca751737e350
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-09-27 20:10:52 +0200

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import React, { Fragment } from 'react';
+
+
+export default function App() {
+
+    const html =
+
+        <Fragment>
+
+            <p>test paragraph</p>
+
+        </Fragment>
+
+
+    return html;}
