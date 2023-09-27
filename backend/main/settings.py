0ac87ebf058961085afc8b1f181fc7c18774b5d0
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-09-27 22:41:59 +0200

    updated settings

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-l2t3$ne-fl4ng4whkoy-)bfp=y)ut^f$1hw_l82a)dcv_$ms!7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost"
+]
 
 
 # Application definition
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "zoo",
+    "rest_framework",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -47,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware"
 ]
 
 ROOT_URLCONF = 'main.urls'
