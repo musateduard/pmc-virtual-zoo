a34c46f43fb59d44403be33f1906293ec1892ca5
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-09-28 20:24:10 +0200

    renamed TestModel to Test; added Animal model

--- backend/zoo/models.py
+++ backend/zoo/models.py
@@ -1,13 +1,26 @@
 # django imports
-from django.db.models import Model, CharField, IntegerField, BooleanField
+from django.db.models import Model, CharField, IntegerField, BooleanField, DateField
 
 
-class TestModel(Model):
+class Test(Model):
 
-    test_char: CharField = CharField(max_length=255, unique=True, blank=False, null=False)
+    test_char: CharField = CharField(max_length=255, blank=False, null=False)
     test_int: IntegerField = IntegerField(blank=False, null=False)
-    test_bool: BooleanField = BooleanField(blank=False, null=False)
+    test_bool: BooleanField = BooleanField(blank=False, null=False, default=False)
+    test_date: DateField = DateField(auto_now=True)
 
     class Meta:
         managed: bool = True
         db_table: str = "test"
+
+
+class Animal(Model):
+
+    name: CharField = CharField(max_length=255, blank=False, null=False)
+    weight: IntegerField = IntegerField(blank=False, null=False)
+    superpower: CharField = CharField(max_length=255, blank=False, null=False)
+    extinct_since: CharField = CharField(max_length=255, blank=False, null=False, default="unknown")
+
+    class Meta:
+        managed: bool = True
+        db_table: str = "animal"
