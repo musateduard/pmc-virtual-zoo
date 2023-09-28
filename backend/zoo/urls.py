f1771d91a6dbdbedfb1afd69e916ed3fea8c8ce3
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-09-28 20:58:09 +0200

    renamed endpoint animal to animals

--- backend/zoo/urls.py
+++ backend/zoo/urls.py
@@ -10,6 +10,6 @@ from rest_framework.routers import DefaultRouter
 router: DefaultRouter = DefaultRouter(trailing_slash=True)
 
 router.register(r"test", TestViewSet)
-router.register(r"animal", AnimalViewSet)
+router.register(r"animals", AnimalViewSet)
 
 urlpatterns: List[URLResolver] = router.urls
