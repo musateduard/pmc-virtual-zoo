e9664d11bbdaab79a6c942f9be01ac9334bf4b11
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-09-28 20:24:33 +0200

    added AnimalSerializer

--- backend/zoo/serializers.py
+++ backend/zoo/serializers.py
@@ -2,7 +2,8 @@
 from rest_framework.serializers import ModelSerializer
 
 # models
-from .models import TestModel
+from .models import Test
+from .models import Animal
 
 # types
 from typing import Dict, List
@@ -10,6 +11,15 @@ from typing import Dict, List
 
 class TestSerializer(ModelSerializer):
 
-    model: type = TestModel
-    fields: List[str] = ["test_char", "test_int", "test_bool"]
-    extra_kwargs: Dict[str, str] = {}
+    class Meta:
+        model: type = Test
+        fields: List[str] = ["test_char", "test_int", "test_bool"]
+        extra_kwargs = {}
+
+
+class AnimalSerializer(ModelSerializer):
+
+    class Meta:
+        model: type = Animal
+        fields: List[str] = ["name", "weight", "superpower", "extinct_since"]
+        extra_kwargs: Dict[str, str] = {}
