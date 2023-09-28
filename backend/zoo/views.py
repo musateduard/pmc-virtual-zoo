c4c2e63cffc6eab8d5f8f9cfb9010235cad9c2c8
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-09-28 20:23:34 +0200

    added AnimalViewSet, updated TestViewSet methods

--- backend/zoo/views.py
+++ backend/zoo/views.py
@@ -1,35 +1,22 @@
-# django imports
 from django.db.models import QuerySet
-
-# rest framework imports
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
 
 # models
-from .models import TestModel
+from .models import Test
+from .models import Animal
 
 # serializers
 from .serializers import TestSerializer
-
-# types
-from typing import List
+from .serializers import AnimalSerializer
 
 
 class TestViewSet(ModelViewSet):
 
     serializer_class: type = TestSerializer
-    allowed_methods: List[str] = ["GET"]
+    queryset: QuerySet = Test.objects.all()
 
 
-    def get_queryset(self) -> QuerySet:
-        """method that returns query set"""
-        return TestModel.objects.all()
+class AnimalViewSet(ModelViewSet):
 
-
-    def list(self, request, *args, **kwargs) -> Response:
-        """get method for all table entries"""
-
-        queryset: QuerySet = self.get_queryset()
-        serializer: TestSerializer = TestSerializer(queryset, many=True)
-
-        return Response(serializer.data)
+    serializer_class: type = AnimalSerializer
+    queryset: QuerySet = Animal.objects.all()
