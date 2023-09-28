f45bec33dc3adc4efa0c4d1023131b2ca5ffff5a
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-09-28 20:18:33 +0200

    added route to test and animal

--- backend/zoo/urls.py
+++ backend/zoo/urls.py
@@ -1,15 +1,15 @@
-# django imports
-from django.urls import URLResolver, path, include
-from rest_framework.routers import DefaultRouter
-
 # viewsets
-from .views import TestViewSet
+from .views import TestViewSet, AnimalViewSet
 
 # types
 from typing import List
+from django.urls import URLResolver
+from rest_framework.routers import DefaultRouter
 
 
 router: DefaultRouter = DefaultRouter(trailing_slash=True)
-router.register(r"", TestViewSet, basename="base name")
 
-urlpatterns: List[URLResolver] = [path(r"", include(router.urls))]
+router.register(r"test", TestViewSet)
+router.register(r"animal", AnimalViewSet)
+
+urlpatterns: List[URLResolver] = router.urls
