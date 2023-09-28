61ebad1d6bb2724cd946d73e24b64a6fb140d7c5
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-09-28 20:59:03 +0200

    updated App with fetch to getAnimals

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,45 @@
 import './App.css';
-import React, { Fragment } from 'react';
+import React, { Fragment, ReactElement, useEffect, useState } from 'react';
 
 
-export default function App() {
+export default function App(): ReactElement {
+
+    const [animals, setAnimals] = useState([]);
+
+
+    const getAnimals: Function = async function(controller: AbortController): Promise<void> {
+
+        try {
+            const response: Response = await fetch("http://localhost:8000/animals/", {signal: controller.signal});
+            const data = await response.json();
+
+            setAnimals(data);}
+
+        catch (error) {
+            console.log(error);}
+
+        return;}
+
+
+    useEffect(
+
+        // effect handler
+        () => {
+            const controller: AbortController = new AbortController();
+            getAnimals(controller);
+            return () => {controller.abort()};},
+
+        // dependency array
+        []);
+
 
     const html =
 
         <Fragment>
 
-            <p>test paragraph</p>
+            <h3>animals</h3>
+
+            {animals.map((item: any, index: number): ReactElement => {return <p key={index}>{item.name}</p>})}
 
         </Fragment>
 
