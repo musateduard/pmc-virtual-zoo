0e3ddd80655ae204056e292928682d3137f47fff
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-10-03 20:52:38 +0200

    added deleteAnimal method

--- frontend/src/components/AnimalItem.tsx
+++ frontend/src/components/AnimalItem.tsx
@@ -6,6 +6,37 @@ import { Delete } from "@mui/icons-material";
 
 export default function AnimalItem(props: {animalData: Animal}): ReactElement {
 
+
+    const deleteAnimal: Function = async function(animalData: Animal): Promise<void> {
+
+        console.log(`deleting animal ${animalData.id} ${animalData.name}`)
+
+        try {
+            console.log(JSON.stringify(animalData));
+            console.log(`http://localhost:8000/animals/${animalData.id}/`);
+
+            const response: Response = await fetch(
+
+                // fetch url
+                `http://localhost:8000/animals/${animalData.id}/`, {
+
+                // request data
+                method: "DELETE"});
+
+            if (response.status >= 200 && response.status < 300) {
+                console.log("animal successfully deleted");}
+
+            else {
+                console.log(response.status);}}
+
+        catch (error) {
+            console.log(error);}
+
+        window.location.reload();
+
+        return;}
+
+
     const html: ReactElement =
 
         <Box
@@ -29,7 +60,8 @@ export default function AnimalItem(props: {animalData: Animal}): ReactElement {
             <Button
                 variant='contained'
                 size='small'
-                endIcon={<Delete />}>
+                endIcon={<Delete />}
+                onClick={(event): void => deleteAnimal(props.animalData)}>
 
                 remove
             </Button>
