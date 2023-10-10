27c61c385dfca5c9048bd5b3648793c5b1318abf
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-10-10 22:15:53 +0200

    added type hints

--- backend/main/asgi.py
+++ backend/main/asgi.py
@@ -1,16 +1,8 @@
-"""
-ASGI config for main project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
+from django.core.handlers.asgi import ASGIHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.production')
 
-application = get_asgi_application()
+application: ASGIHandler = get_asgi_application()
--- backend/main/wsgi.py
+++ backend/main/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for main project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.handlers.wsgi import WSGIHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.production')
 
-application = get_wsgi_application()
+application: WSGIHandler = get_wsgi_application()
